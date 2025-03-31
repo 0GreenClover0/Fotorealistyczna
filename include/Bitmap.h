214687825f94d2c2d0bf8d132111da19abbcf745
Mikolaj Przybylski <clovers02123@gmail.com>
2025-03-31 19:58:25 +0200

    Add Bitmap class holding pixel color data

---
+++ include/Bitmap.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "Vector.h"
+
+class Bitmap
+{
+public:
+    Bitmap(int width, int height);
+
+    std::vector<std::vector<Vector>> data = {};
+    int width = 0;
+    int height = 0;
+};
---
+++ src/Bitmap.cpp
@@ -0,0 +1,11 @@
+﻿#include "Bitmap.h"
+
+Bitmap::Bitmap(int width, int height) : width(width), height(height)
+{
+    data.resize(height);
+
+    for (int i = 0; i < height; i++)
+    {
+        data[i].resize(width);
+    }
+}
