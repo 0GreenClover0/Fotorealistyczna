04f131998adbe0fa8ba4886c331d282d145b4752
Mikolaj Przybylski <clovers02123@gmail.com>
2025-04-05 16:39:13 +0200

    Interval: Add class

---
+++ include/Interval.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+
+#include <cfloat>
+
+class Interval
+{
+public:
+    float min;
+    float max;
+
+    Interval()
+    {
+        min = FLT_MAX;
+        max = FLT_MIN;
+    }
+
+    Interval(const float min, const float max) : min(min), max(max)
+    {
+    }
+
+    Interval(const Interval& a, const Interval& b)
+    {
+        min = a.min <= b.min ? a.min : b.min;
+        max = a.max >= b.max ? a.max : b.max;
+    }
+
+    float size() const
+    {
+        return max - min;
+    }
+
+    bool contains(float x) const
+    {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(float x) const
+    {
+        return min < x && x < max;
+    }
+
+    Interval expand(float const delta) const
+    {
+        float const padding = delta / 2.0f;
+
+        return Interval(min - padding, max + padding);
+    }
+
+    static Interval const empty;
+    static Interval const whole;
+};
+
+inline Interval operator+(Interval const& interval, float const displacement)
+{
+    return Interval(interval.min + displacement, interval.max + displacement);
+}
+
+inline Interval operator*(Interval const& interval, float const scale)
+{
+    return Interval(interval.min * scale, interval.max * scale);
+}
+
+inline Interval operator+(float const displacement, Interval const& interval)
+{
+    return interval + displacement;
+}
