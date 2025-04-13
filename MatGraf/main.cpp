6536591c9fd866f6ae0c356a49b55a01fd88a5d7
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-04-13 21:38:21 +0200

    Main: Set up a simple Cornell Box

--- MatGraf/main.cpp
+++ MatGraf/main.cpp
@@ -3,13 +3,16 @@
 
 #include "Camera.h"
 #include "Global.h"
+#include "Plane.h"
 #include "PointLight.h"
 #include "Sphere.h"
+// #include <Windows.h>
+// #pragma comment(lib, "shell32.lib")
 
 int main()
 {
-    std::shared_ptr<Bitmap> bitmap = std::make_shared<Bitmap>(100, 100);
-    Camera camera = Camera(bitmap, 50, 1);
+    std::shared_ptr<Bitmap> bitmap = std::make_shared<Bitmap>(300, 300);
+    Camera camera = Camera(bitmap, 16, 1);
     std::shared_ptr<HittableList> world = std::make_shared<HittableList>();
 
     camera.verticalFov = 90.0f;
@@ -17,16 +20,31 @@ int main()
     camera.lookAt = Vector(0.0f, 0.0f, 2.0f);
     camera.vUp = Vector(0.0f, 1.0f, 0.0f);
 
-    std::shared_ptr<Material> mat1 = std::make_shared<Material>(Vector(0.0f, 0.0f, 0.2f), Vector(1.5f, 1.0f, 1.0f), 5.0f);
-    std::shared_ptr<Material> mat2 = std::make_shared<Material>(Vector(0.0f, 0.0f, 0.2f), Vector(1.5f, 1.0f, 1.0f), 50.0f);
+    std::shared_ptr<Material> mat1 = std::make_shared<Material>(Vector(1.0f, 0.0f, 0.0f), Vector(1.0f, 1.0f, 1.0f), 50.0f);
+    std::shared_ptr<Material> mat2 = std::make_shared<Material>(Vector(0.0f, 0.0f, 1.0f), Vector(1.0f, 1.0f, 1.0f), 5.0f);
+    std::shared_ptr<Material> matWallWhite = std::make_shared<Material>(Vector(1.0f, 1.0f, 1.0f), Vector(0.005f, 0.005f, 0.005f), 5.0f);
+    std::shared_ptr<Material> matWallRed = std::make_shared<Material>(Vector(1.0f, 0.0f, 0.0f), Vector(0.005f, 0.005f, 0.005f), 5.0f);
+    std::shared_ptr<Material> matWallBlue = std::make_shared<Material>(Vector(0.0f, 0.0f, 1.0f), Vector(0.005f, 0.005f, 0.005f), 5.0f);
 
-    std::shared_ptr<Sphere> sphere1 = std::make_shared<Sphere>(Vector(-4.0f, 0.0f, -3.0f), 4.0f, mat1);
-    std::shared_ptr<Sphere> sphere2 = std::make_shared<Sphere>(Vector(2.0f, 0.0f, 0.0f), 4.0f, mat2);
+    std::shared_ptr<Plane> plane1 = std::make_shared<Plane>(Vector(0.0f, 0.0f, 0.0f), Vector(0.0f, 0.0f, 1.0f), matWallWhite);
+    std::shared_ptr<Plane> plane2 = std::make_shared<Plane>(Vector(0.0f, 4.0f, 0.0f), Vector(0.0f, -1.0f, 0.0f), matWallWhite);
+    std::shared_ptr<Plane> plane3 = std::make_shared<Plane>(Vector(0.0f, -4.0f, 0.0f), Vector(0.0f, 1.0f, 1.0f), matWallWhite);
+    std::shared_ptr<Plane> plane4 = std::make_shared<Plane>(Vector(-4.0f, 0.0f, 0.0f), Vector(1.0f, 0.0f, 1.0f), matWallRed);
+    std::shared_ptr<Plane> plane5 = std::make_shared<Plane>(Vector(4.0f, 0.0f, 0.0f), Vector(-1.0f, 0.0f, 1.0f), matWallBlue);
+
+    std::shared_ptr<Sphere> sphere1 = std::make_shared<Sphere>(Vector(-1.0f, 0.0f, 4.0f), 1.0f, mat1);
+    std::shared_ptr<Sphere> sphere2 = std::make_shared<Sphere>(Vector(1.2f, -0.5f, 2.0f), 1.0f, mat2);
+
     world->addToWorld(sphere1);
     world->addToWorld(sphere2);
+    world->addToWorld(plane1);
+    world->addToWorld(plane2);
+    world->addToWorld(plane3);
+    world->addToWorld(plane4);
+    world->addToWorld(plane5);
 
-    lights.push_back(std::make_shared<PointLight>(Vector(0.0f, 1.0f, 6.0f), Vector(4.0f, 4.0f, 4.0f), Vector(0.3f, 0.0f, 0.0f)));
-    lights.push_back(std::make_shared<PointLight>(Vector(0.0f, 6.0f, 3.0f), Vector(0.0f, 2.0f, 0.0f), Vector(0.0f, 0.4f, 0.0f)));
+    lights.push_back(std::make_shared<PointLight>(Vector(0.0f, 3.8f, 4.0f), Vector(0.4f, 0.4f, 0.4f), Vector(0.2f, 0.2f, 0.2f)));
+    lights.push_back(std::make_shared<PointLight>(Vector(0.0f, -3.8f, 4.0f), Vector(0.4f, 0.4f, 0.4f), Vector(0.2f, 0.2f, 0.2f)));
 
     camera.isOrthographic = false;
     camera.render(world);
@@ -55,6 +73,7 @@ int main()
     }
 
     output.close();
+    // ShellExecuteW(nullptr, L"open", L"output.ppm", nullptr, L".", SW_SHOW);
 
     std::cout << "Done\n";
 }
